28dd81e72edd53ace1c40cdfe173a0e2ff17cf95
Matthew Voss <mcvoss@wisc.edu>
2022-11-27 20:54:08 -0600

    cleaned up the logistic regression file

--- Logistic.py
+++ Logistic.py
@@ -2,14 +2,9 @@ from main import LymeDisease, Grid_Search
 from sklearn.linear_model import LogisticRegression
 
 l = LymeDisease()
-#
-# model = LogisticRegression(max_iter=10000).fit(l.x_train, l.y_train)
-# acc = model.score(l.x_valid, l.y_valid)*100
-# print(f'The model accuracy is {acc:.2f}%')
+
 estimator = LogisticRegression()
-param_grid = {'max_iter': 10000}
-print = Grid_Search(estimator=estimator, cv=10, param_grid=param_grid, x_train=l.x_train, y_train=l.y_vtrain,
+param_grid = {'max_iter': [10000]}
+print = Grid_Search(estimator=estimator, cv=10, param_grid=param_grid, x_train=l.x_train, y_train=l.y_train,
                     x_valid=l.x_valid, y_valid=l.y_valid)
-"""
-Accuracy is pretty low and we desire an accuracy at least in the 80 percent range. 
-"""
+# Accuracy is pretty poor at just 71  percent validation accuracy.
