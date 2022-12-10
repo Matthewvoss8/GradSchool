bce480ed27d80d3acf9faa1c6353a6494ffcebc4
Matthew Voss <mcvoss@wisc.edu>
2022-12-10 14:46:14 -0600

    Specified a random state and got new accuracy

--- Logistic.py
+++ Logistic.py
@@ -3,8 +3,8 @@ from sklearn.linear_model import LogisticRegression
 
 l = LymeDisease()
 
-estimator = LogisticRegression()
-param_grid = {'max_iter': [10000]}
-print = Grid_Search(estimator=estimator, cv=10, param_grid=param_grid, x_train=l.x_train, y_train=l.y_train,
-                    x_valid=l.x_valid, y_valid=l.y_valid)
-# Accuracy is pretty poor at just 71  percent validation accuracy.
+estimator = LogisticRegression(random_state=450)
+param_grid = {'max_iter': [1000]}
+Grid_Search(estimator=estimator, cv=10, param_grid=param_grid, x_train=l.x_train, y_train=l.y_train,
+            x_valid=l.x_valid, y_valid=l.y_valid)
+# Accuracy is ok at just 74.8% validation accuracy.
