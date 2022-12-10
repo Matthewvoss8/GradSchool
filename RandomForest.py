700b3695ac721fe9888425d3fe717d7d8d4ff697
Matthew Voss <mcvoss@wisc.edu>
2022-12-10 14:46:57 -0600

    rewrote to use the Grid_Search Class

--- RandomForest.py
+++ RandomForest.py
@@ -1,35 +1,24 @@
 from sklearn.ensemble import RandomForestClassifier
 from main import LymeDisease, Grid_Search
-from sklearn.model_selection import GridSearchCV
 import pandas as pd
 
 l = LymeDisease()
 random_forest = RandomForestClassifier(class_weight='balanced')
-param_grid = {'n_estimators': pd.Series(range(1, 11)).apply(lambda x: x*10).to_list(),
-                      'criterion': ['gini', 'entropy'],
-                      'max_depth': [1, 2, 5, 10, 20],
-                      'min_samples_leaf': list(range(1,6))
-             }
-rf = GridSearchCV(
-            estimator=random_forest,
-            param_grid=param_grid
-)
-rf.fit(l.x_train, l.y_train)
-print('Best Random Forest', rf.best_params_)
+
+param_grid = {'n_estimators': pd.Series(range(1, 11)).apply(lambda x: x * 10).to_list(),
+              'criterion': ['gini', 'entropy'],
+              'max_depth': [1, 2, 5, 10, 20],
+              'min_samples_leaf': list(range(1, 6))
+              }
+Grid_Search(estimator=random_forest, cv=10, param_grid=param_grid, x_train=l.x_train, y_train=l.y_train,
+            x_valid=l.x_valid, y_valid=l.y_valid)
+
 param_grid = {'n_estimators': list(range(75, 86)),
-                      'criterion': ['gini', 'entropy'],
-                      'max_depth': list(range(2, 9)),
-                      'min_samples_leaf': list(range(1, 3))}
-rf = GridSearchCV(
-            estimator=random_forest,
-            param_grid=param_grid,
-            cv=10,
-            scoring='accuracy'
-)
-rf.fit(l.x_train, l.y_train)
-print('Best Random Forest parameters %s' % rf.best_params_)
-print('Training accuracy for random forest is %.2f%%' % (rf.best_score_*100))
-print('Validation accuracy for random forest is %.2f%%' % (rf.best_estimator_.score(l.x_valid, l.y_valid) * 100))
-print = Grid_Search(estimator = random_forest, cv=10, param_grid=param_grid, x_train=l.x_train,
-                    y_train=l.y_train, x_valid=l.x_valid, y_valid=l.y_valid)
-# using the param_grid, still best validation accuracy at 78%
+              'criterion': ['gini', 'entropy'],
+              'max_depth': list(range(2, 9)),
+              'min_samples_leaf': list(range(1, 3))}
+
+Grid_Search(estimator=random_forest, cv=10, param_grid=param_grid, x_train=l.x_train, y_train=l.y_train,
+            x_valid=l.x_valid, y_valid=l.y_valid)
+
+# using the param_grid, still best validation accuracy at 77%
